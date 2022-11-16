d5b9326cdf80279ef207c05462f04ec77017bd76
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-11-16 22:18:31 +0100

    [general] wip timeline

---
+++ src/components/DynamiqueTimeline.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="timeline">
+    <h2 class="timeline__date">date</h2>
+    <div>
+      timeline
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+.timeline {
+  position: fixed;
+  right: 37px;
+  top: 32px;
+  text-align: right;
+
+  &__date {
+    font-size: 40px;
+  }
+}
+</style>
